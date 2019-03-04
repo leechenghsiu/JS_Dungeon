d530ae329608d51d317bedc0ae024d7cc5bfc0ef
lkm981324 <matthew6303@gmail.com>
2019-03-04 15:48:38 +0800

    Modify Router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 import Boss1 from './1F_BOSS_9x9/components/Boss1';
 import Boss2 from './2F_BOSS_CLOCK/components/Boss2';
@@ -13,6 +13,9 @@ ReactDOM.render(
         <Route path="/3F_BOSS_CALCULATOR" component={Boss3} />
         <Route path="/2F_BOSS_CLOCK" component={Boss2} />
         <Route path="/1F_BOSS_9x9" component={Boss1} />
+        <Route path="/">
+          <Redirect to={{pathname: "/1F_BOSS_9x9"}} />
+        </Route>
       </Switch>
     </div>
   </BrowserRouter>,
